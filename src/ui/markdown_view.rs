d63fe8e232bce43a3f1bf2368ec8d705c6cc48d5
Jeffrey Gordon <jeff@jdgwf.com>
2023-03-20 08:43:09 -0500

    Frontend speedup - fixed logout issue

--- src/ui/markdown_view.rs
+++ src/ui/markdown_view.rs
@@ -1,5 +1,8 @@
 // use gloo_console::log;
-use markdown_to_html;
+// use block_rule;
+// use core_rule;
+// use inline_rule;
+use markdown_it;
 use web_sys::{Element, Node};
 use yew::{function_component, Html, Properties};
 #[derive(Properties, PartialEq)]
@@ -12,7 +15,17 @@ pub fn MarkdownView(props: &Props) -> Html {
     // add a final newline so the parser lib doesn't gripe.
     let markdown_text = props.markdown.to_owned() + &"\n";
 
-    let inner_html = markdown_to_html::markdown(&markdown_text);
+    let md = &mut markdown_it::MarkdownIt::new();
+
+    // add commonmark syntax, you almost always want to do that
+    markdown_it::plugins::cmark::add(md);
+
+    // add custom three rules described above
+    // inline_rule::add(md);
+    // block_rule::add(md);
+    // core_rule::add(md);
+
+    let inner_html = md.parse(&markdown_text).render();
 
     let div: Element = gloo_utils::document().create_element("div").unwrap();
     div.set_inner_html(&inner_html);
